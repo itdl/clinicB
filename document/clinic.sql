ec9ce73e99e51f7a393770260a54de6581d3f98d
songshijun <songshijun@atme8.cn>
2017-03-28 21:05:41 +0800

    药品管理

--- document/clinic.sql
+++ document/clinic.sql
@@ -10,10 +10,59 @@ Target Server Type    : MYSQL
 Target Server Version : 50610
 File Encoding         : 65001
 
-Date: 2017-03-14 22:14:38
+Date: 2017-03-28 21:04:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for clinic_mgr_drug
+-- ----------------------------
+DROP TABLE IF EXISTS `clinic_mgr_drug`;
+CREATE TABLE `clinic_mgr_drug` (
+  `code` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '商品编码',
+  `name` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '商品名称',
+  `type` int(3) NOT NULL COMMENT '商品类型',
+  `attention` varchar(255) COLLATE utf8_bin DEFAULT '' COMMENT '注意事项',
+  `price` double(10,2) DEFAULT NULL COMMENT '处方价格',
+  `format` varchar(20) COLLATE utf8_bin DEFAULT '' COMMENT '规格',
+  `prd_date` timestamp NULL DEFAULT NULL COMMENT '生产日期',
+  `valid_date` timestamp NULL DEFAULT NULL COMMENT '有效日期',
+  `shape` varchar(50) COLLATE utf8_bin DEFAULT '' COMMENT '性状',
+  `counts` int(8) DEFAULT '0' COMMENT '药品数量',
+  `prd_phone` varchar(20) COLLATE utf8_bin DEFAULT '' COMMENT '生产厂商电话',
+  `prd_firm` varchar(255) COLLATE utf8_bin DEFAULT '' COMMENT '厂家地址',
+  `uses` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '用途',
+  `dosage` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '用量',
+  `remark` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`type`,`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='药品表';
+
+-- ----------------------------
+-- Records of clinic_mgr_drug
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for clinic_mgr_order
+-- ----------------------------
+DROP TABLE IF EXISTS `clinic_mgr_order`;
+CREATE TABLE `clinic_mgr_order` (
+  `order_no` varchar(40) COLLATE utf8_bin NOT NULL COMMENT '订单号码',
+  `order_date` timestamp NULL DEFAULT NULL COMMENT '订单生成日期',
+  `order_status` int(1) NOT NULL COMMENT '订单状态',
+  `drug_name` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '药品名称',
+  `drug_count` int(8) DEFAULT NULL COMMENT '药品数量',
+  `drug_type` int(3) DEFAULT NULL COMMENT '药品类型',
+  `pay_flag` int(1) DEFAULT NULL COMMENT '支付标志',
+  `cus_ser_status` varchar(20) COLLATE utf8_bin DEFAULT '' COMMENT '售后状态',
+  `unit_price` double(10,2) DEFAULT NULL COMMENT '单价',
+  `all_price` double(10,2) DEFAULT NULL COMMENT '总价格',
+  PRIMARY KEY (`order_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of clinic_mgr_order
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for clinic_mgr_user
