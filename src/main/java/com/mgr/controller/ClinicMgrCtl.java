c467953e91b011f4295f75be2fae2ba4bb737694
junjun <2051340319@qq.com>
2017-04-27 23:19:13 +0800

    代码提交

--- src/main/java/com/mgr/controller/ClinicMgrCtl.java
+++ src/main/java/com/mgr/controller/ClinicMgrCtl.java
@@ -3,6 +3,8 @@ package com.mgr.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 药品管理控制层
@@ -20,5 +22,12 @@ public class ClinicMgrCtl {
     @RequestMapping(value="/{html}")
     public String htmlGet(@PathVariable String html){
         return html;
+    }
+
+    @RequestMapping(value="/admin",method = RequestMethod.GET)
+    public ModelAndView mgrAdmin(ModelAndView model){
+
+        model.setViewName("mgr/mgr-admin");
+        return model;
     }
 }
--- src/main/webapp/static/appjs/login.js
+++ src/main/webapp/static/appjs/login.js
@@ -4,12 +4,6 @@ var Login = (function($){
         $("#login").on('click',function(){
             login();
         })
-        // document.onkeydown = function (event) {
-        //     var e = event || window.event || arguments.callee.caller.arguments[0];
-        //     if (e && e.keyCode == 13) {
-        //         $("#loginBtn").click();
-        //     }
-        // };
     };
     /**
      * 登录
